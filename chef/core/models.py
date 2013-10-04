7b72b6ff322afb542b309c3f257239aea0222981
Sanket Saurav <sanketsaurav@gmail.com>
2013-10-04 23:16:51 +0530

    Modified User model.

--- chef/core/models.py
+++ chef/core/models.py
@@ -7,52 +7,41 @@ class User(models.Model):
 
 	# Possible authorization levels of users in Chef
 	AUTH_LEVELS = (
-	(0, 'ADMIN'),
-	(1, 'MOD'),
-	(2, 'ATTENDEE')
+	('ADMIN', 'Administrator'),
+	('MOD', 'Moderator'),
+	('USR', 'Attendee')
 	)
 
 	user_id = models.CharField(
 		max_length=128, 
-		blank=False, 
-		null=False
 		)
 	
 	first_name = models.CharField(
 		max_length=1024, 
-		blank=False, 
-		null=False
 		)
 
 	last_name = models.CharField(
 		max_length=1024, 
-		blank=True, 
-		null=True
 		)
 
-	email = models.EmailField()
+	email = models.EmailField(
+
+		)
 
 	user_type = models.CharField(
 			choices=AUTH_LEVELS,
 			default=2,
-			blank=False,
-			null=False
 			)
 
 	added = models.DateTimeField(
 			auto_now_add=True,
-			blank=False,
-			null=False
 			)
 
 	organization = models.CharField(
 			max_length=1024,
-			null=False,
-			blank=False
 			)
 
 	campushash_uid = models.CharField(
 		max_length=1024,
-		null=True,
 		blank=True
 		)
