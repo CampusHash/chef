67bc7a7943297466fe0a9415d1d77f21545fe3cd
Sanket Saurav <sanketsaurav@gmail.com>
2013-11-09 20:30:04 +0530

    Added Event model.

--- chef/core/models.py
+++ chef/core/models.py
@@ -17,3 +17,10 @@ class Profile(models.Model):
 	user_type = models.CharField(max_length=3, choices=AUTH_LEVELS, default='USR')
 	organization = models.CharField(max_length=1024)
 	campushash_uid = models.CharField(max_length=1024, blank=True)
+
+class Event(models.Model):
+	"""
+	Event definitions. An admin can create / update / delete an event.
+	A default locally created can be registered with CampusHash Event Central.
+	Doing do needs an API key.
+	"""
