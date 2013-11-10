7144a0cdbff3eb70b497401083a41de3594c0fe8
Sanket Saurav <sanketsaurav@gmail.com>
2013-11-10 18:32:58 +0530

    Fixed typo.

--- chef/core/models.py
+++ chef/core/models.py
@@ -10,17 +10,18 @@ class Profile(models.Model):
 	AUTH_LEVELS = (
 		('ADM', 'Administrator'),
 		('MOD', 'Moderator'),
-		('USR', 'Attendee')
+		('ATT', 'Attendee')
 	)
 
 	user = models.ForeignKey(User)
-	user_type = models.CharField(max_length=3, choices=AUTH_LEVELS, default='USR')
+	user_type = models.CharField(max_length=3, choices=AUTH_LEVELS, default='ATT')
 	organization = models.CharField(max_length=1024)
 	campushash_uid = models.CharField(max_length=1024, blank=True)
 
 class Event(models.Model):
 	"""
-	Event definitions. An admin can create / update / delete an event.
+	Event definitions. An admin can create/update/delete an event.
 	A default locally created can be registered with CampusHash Event Central.
 	Doing do needs an API key.
 	"""
+	name = models.CharField(max_length=1024)
